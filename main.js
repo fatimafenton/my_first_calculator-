8bcc1e227b735f12b230e9b357b9621b3123605a
fatima <fatimafenton92@gmail.com>
2016-04-25 16:05:35 +0200

    my first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+console.log('main.js') 
+	function addtoscreen(num)
+	{
+		document.getElementById('screen2').value +=num;
+} 
+function operation(op){
+	if (op == '+') {
+		console.log('plus');
+	}
+	else if (op == '-') {
+		console.log('minus');
+	}
+	else if(op == '*'){
+		console.log('multiplation')
+	}
+	else if (op == '/') {
+		console.log('divison')
+	}
+	else if(op == '='){
+		console.log('equal')
+	}
+	else if(op == 'reset'){
+		console.log('clear')
+	}
+}
+
+ 
+
+
+	
+
+
